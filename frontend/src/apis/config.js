c51bde9e95aeab94cd1281516be01094b60b9b91
Raj305173 <krishagrawal343@gmail.com>
2023-03-27 20:35:57 +0530

    frontend done

---
+++ frontend/src/apis/config.js
@@ -0,0 +1,7 @@
+
+//export const base_url = `http://localhost:9002`;  //for locally checking
+
+export const base_url= `https://carddata.onrender.com/`;  //server uploaded on render
+
+
+ // export const base_url = `https://convin-fa-backend.vercel.app`;
